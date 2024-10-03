b91e8deb246609dc080f032228308fbb70b07279
gary_surf7 <garynova781@gmail.com>
2024-10-03 20:35:44 +0800

    improvement for the news site

---
+++ news.js
@@ -0,0 +1,43 @@
+// news.js
+const news = {
+    news1: {
+        title: "g-Pico finally getting ready for the market!",
+        subtitle: "Finally, g-Pico is Here!",
+        subtitleColor: "#007bff", // 設置副標題顏色，例如藍色
+        content: `
+            <p>After two years of cooking, g-Pico has finally launched, and we couldn't be more excited!</p>
+            <p>As engineers in Taiwan, we've often found ourselves bogged down by routine tasks, searching for ways to improve
+            efficiency. Through data collection and analysis in our daily work, we began to truly appreciate the power
+            of automation. This sparked a passion in us to develop and promote automated solutions that could make
+            life easier for everyone.</p>
+
+            <p>Starting with no knowledge of Python, we've spent the past two
+             years working relentlessly to build and refine the g-Pico system.
+              Today, we're thrilled to see it finally ready to launch, opening 
+              doors to new possibilities in automation.
+            </p>
+
+            <p>We are filled with hope for the future and eager to share the 
+            benefits of automation with others. Our goal is to help everyone 
+            lead more efficient and fulfilling lives through technology. This 
+            is just the beginning!
+            </p>
+
+            <p>We are deeply grateful to everyone who has helped us along the 
+            way, as well as the online resources that supported us. We will try 
+            our best to make people's lives better.
+            </p>
+        `,
+        image: "party_time_pico_release.webp"
+    },
+    news2: {
+        title: "Partnership announcement",
+        subtitle: "New Partnership!",
+        subtitleColor: "#ff5733", // 設置副標題顏色，例如橙色
+        content: `
+            <p>We are excited to announce our new partnership...</p>
+        `,
+        image: "partnership_image.webp"
+    },
+    // 新增其他新聞項目
+};
